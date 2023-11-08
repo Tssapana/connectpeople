01f45e259df83ae1872a44c5a0d09146fb3efa88
Tssapana <sapnalama456@gmail.com>
2023-11-08 00:11:12 -0600

    initial commit

---
+++ post/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Post(models.Model):
+    content=models.CharField(max_length=400)
+    image=models.ImageField(upload_to="posts",blank=True,null=True)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.author.username} created post at {self.updated}"
+    
+    class Meta:
+        ordering=["-updated"]
+    
+class Comment(models.Model):
+    comment=models.CharField(max_length=200)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.user.username} commented post at {self.updated}"
+    
+    class Meta:
+        ordering=["-updated"]
+
+
+
+    
+    
---
+++ profiles/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import FileExtensionValidator
+
+# Create your models here.
+
+STATUS_CHOICE=(
+    ("send","send"),
+    ("accepted", "accepted")
+)
+
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    address=models.CharField(max_length=100)
+    bio=models.TextField(blank=True)
+    profile_pic=models.ImageField(upload_to="profile",validators=[FileExtensionValidator(['png','jpg','jpeg'])])
+    friends= models.ManyToManyField(User, blank=True, related_name="friends")
+
+    def __str__(self):
+        return f"{self.user.username} profile has been created "
+    
+class Relationship(models.Model):
+    sender= models.ForeignKey(Profile,on_delete=models.CASCADE,related_name="sender")
+    receiver=models.ForeignKey(Profile, on_delete=models.CASCADE,related_name="receiver")
+    status=models.CharField(max_length=8,choices=STATUS_CHOICE)
+
+    def __str__(self):
+        return f"{self.sender.user.username} has sent the request to {self.receiver.user.username}"
+    
